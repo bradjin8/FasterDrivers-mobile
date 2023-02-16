abeace8859ddbea3c8bae7241ea69b0e344138a8
sallarrezaie <91070355+sallarrezaie@users.noreply.github.com>
2023-02-16 15:46:43 +0700

    Sort by Category on Menu

--- backend/restaurants/utils.py
+++ backend/restaurants/utils.py
@@ -3,3 +3,11 @@ def sort_by_type(results):
     for item in results:
         res.setdefault(item['type'], []).append(item)
     return res
+
+def sort_by_category(results):
+    menu_data = results.pop('dishes', None)
+    res = {}
+    if menu_data:
+        for item in menu_data:
+            res.setdefault(item['category'], []).append(item)
+    return res
